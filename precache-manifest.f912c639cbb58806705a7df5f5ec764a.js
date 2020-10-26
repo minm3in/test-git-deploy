26bd4db5eda603f0b83b839c785ed90ba11d4e9b
minm3in <minmein86@gmail.com>
2020-10-26 16:04:47 +0800

    Updates

---
+++ precache-manifest.f912c639cbb58806705a7df5f5ec764a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8096525023991eb43634c4e943e34f",
+    "url": "/test-git-deploy/index.html"
+  },
+  {
+    "revision": "a2462e197b66d9616cf7",
+    "url": "/test-git-deploy/static/css/main.f68cbbf8.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/test-git-deploy/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-git-deploy/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2462e197b66d9616cf7",
+    "url": "/test-git-deploy/static/js/main.ea51f3d4.chunk.js"
+  },
+  {
+    "revision": "aae5b5ece2e713028915",
+    "url": "/test-git-deploy/static/js/runtime-main.673045f4.js"
+  }
+]);
